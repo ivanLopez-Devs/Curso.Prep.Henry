3d77ec51b60e6bd01b7b8d806abcaef82e9c131d
Iván Alejandro López <ivanlopez.devs@gmail.com>
2022-01-29 00:43:14 -0300

    Homework JS6 finalizado

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,12 +8,45 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  
+  // class Usuario {
+  //   constructor(opciones) {
+  //     this.usuario = opciones.usuario;
+  //     this.nombre = opciones.nombre;
+  //     this.email = opciones.email;
+  //     this.password = opciones.password;
+  //   }
+  //   saludar() {
+  //     return "Hola, mi nombre es" + " " + this.nombre;
+  //   }
+  // }
+
+
+  // return Usuario;
+  
+  class Usuario{                // claramente vemos una clase ES6 que tiene un function constructor que recibe
+    constructor(opciones){      // el objeto opciones, el cual tiene las propiedades "usuario, nombre, mail,pasword".
+      this.usuario = opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.password = opciones.password;
+    }
+    saludar() {
+      return "Hola, mi nombre es" + " " + this.nombre; 
+    }
+  }
+
+  return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+
+  Constructor.prototype.saludar = function() {
+    return "Hello World!";
+  }
 }
 
 function agregarStringInvertida() {
@@ -22,6 +55,14 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+
+  String.prototype.reverse = function() {
+    var invertida = "";                   // crear una variable auxiliar para colocar el string invertido.
+    for(var i = this.length; i > 0; i--) {   // este for va de atras para adelante, tener en cuenta que
+      invertida = invertida + this[i-1];     // length me da el largo del string, ej: "toni" = 4, pero 
+    }                                        // i comienza en 0, t=0 o=1 n=2 i=3, entonces this debe iniciar
+    return invertida;                        // en i = 3, para ello se pone [i-1];
+  }
 }
 
 // ---------------------------------------------------------------------------//
@@ -36,9 +77,20 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
-      // Crea el constructor:
+    constructor(nombre, apellido, edad, domicilio) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+    }
 
+    detalle(){
+     return {
+      Nombre :this.nombre,
+      Apellido: this.Apellido,
+      Edad: this.edad,
+      Domicilio: this.domicilio,
+      }
     }
 }
 
@@ -46,11 +98,18 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+
+  var nuevaPersona1 = new Persona(nombre, apellido, edad, dir);
+  return nuevaPersona1;
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+
+  Persona.prototype.datos = function(){
+    return this.nombre+ ", " +this.edad+" años";
+  }
 }
   
 
